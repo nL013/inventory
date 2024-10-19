530f18d2cecec9697b97af2b2f160e47693ff1f5
nL013 <dinamit10d@gmail.com>
2024-10-19 18:49:04 +0500

    commit

---
+++ main.js
@@ -0,0 +1,45 @@
+
+let productType = document.getElementById("type-select")
+let productName = document.getElementById("product-name")
+let productCount = document.getElementById("product-count")
+
+
+let addBtn = document.querySelector(".add-product")
+let clearBtn = document.querySelector(".clear-list")
+
+let productList = document.querySelector(".products-list")
+let arrayProducts =[]
+
+
+addBtn.addEventListener("click",addProduct)
+
+
+function addProduct(){
+    let productTypeValue= productType.value
+    let productNameValue= productName.value
+    let productCountValue = productCount.value
+
+    let productItem= `
+    <div class="product-item">
+        Тип продукта ${productTypeValue} Название:${productNameValue} Количество:${productCountValue}
+    </div>`
+    let products =""
+    arrayProducts.push(productItem)
+
+    
+    arrayProducts.forEach((e)=>{
+        products=products+e
+    })
+    productList.innerHTML=products
+}
+
+
+clearBtn.addEventListener("click", clearInputs);
+
+function clearInputs() {
+  productType.value = "";
+  productName.value = "";
+  productCount.value = "";
+  productList.innerHTML = ""; 
+  arrayProducts = []; 
+}
